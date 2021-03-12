5a143b3f257d19d605d256833428552d13446ec1
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-12 17:24:18 +0100

    added base of admin

--- src/views/Admin/Admin.js
+++ src/views/Admin/Admin.js
@@ -1,17 +1,40 @@
 import "./styles.css";
-import { useState } from "react";
+import Api from "../../Api/fileAPI";
+import { useState, useEffect } from "react";
 import AdminChat from "../../Components/AdminChat";
 import AdminFiles from "../../Components/AdminFiles";
 import AdminPi from "../../Components/AdminPi";
 
-export default function Admin() {
+export default function Admin({ socket }) {
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    socket.on("loadNewFile", async () => {
+      try {
+        const res = await Api.get("/local");
+        setFiles(res.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
+  useEffect(async () => {
+    try {
+      const res = await Api.get("/local");
+      setFiles(res.data.data);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+  console.log(files);
   return (
     <div className="admin-view">
       <div>
         <h1>Admin View</h1>
       </div>
-      <AdminChat />
-      <AdminFiles />
+      <AdminChat socket={socket} />
+      <AdminFiles files={files} socket={socket} />
       <AdminPi />
     </div>
   );
