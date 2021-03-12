276d7a224e8dc2d2463bb2ead7e30b3b0afcc63f
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-12 10:14:27 +0100

    added delete-all handler

--- src/Components/AdminFiles/index.js
+++ src/Components/AdminFiles/index.js
@@ -1,6 +1,14 @@
 import "./styles.css";
+import Api from "../../Api/fileAPI";
 
 export default function AdminFiles() {
+  const deleteAll = async () => {
+    try {
+      await Api.delete("/local");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="admin-files one flex center message">
       <div>
@@ -9,7 +17,12 @@ export default function AdminFiles() {
         <span className="stack">Files uploaded:</span>
         <span className="stack">Used Space:</span>
         <span className="stack">Available Space:</span>
-        <button className="flex center danger pseudo">Delete All Files</button>
+        <button
+          onClick={() => deleteAll()}
+          className="flex center danger pseudo"
+        >
+          Delete All Files
+        </button>
       </div>
     </div>
   );
