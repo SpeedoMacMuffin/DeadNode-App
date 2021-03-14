3663da5b50af84f8880c813363f260fbdb140b7f
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-14 21:14:14 +0100

    cleaned up old useEffect

--- src/Components/AdminChat/index.js
+++ src/Components/AdminChat/index.js
@@ -1,41 +1,34 @@
 import "./styles.css";
 import FormSure from "../FormSure";
 import Api from "../../Api/fileAPI";
-import { useState, useEffect } from "react";
-export default function AdminChat({ socket, messages }) {
+import { useState } from "react";
+
+export default function AdminChat({ socket, messages, setMessages }) {
   const [message, setMessage] = useState("Delete All Messages");
 
   const deleteHandler = () => {
-    socket.emit("delete");
-    setMessage("Messages deleted!");
-    setTimeout(() => {
-      setMessage("Delete All Messages");
-    }, 3000);
+    if (messages > 0) {
+      socket.emit("delete");
+      setMessage("Messages deleted!");
+      setTimeout(() => {
+        setMessage("Delete All Messages");
+        setMessages(0);
+      }, 3000);
+    } else {
+      setMessage("Messages already empty");
+      setTimeout(() => {
+        setMessage("Delete All Messages");
+      }, 3000);
+    }
   };
-  // useEffect(() => {
-  //   socket.emit("room", { room: "admin" });
-  //   socket.on("loadNewFile", async () => {
-  //     try {
-  //       const res = await Api.get("/local");
-  //       setFiles(res.data.data);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   });
-  //   socket.on("history", (msgArray) => {
-  //     setMessages((messages) => [...messages, msgArray]);
-  //   });
-  // });
-  // console.log(messages);
 
   return (
-    <div className="flex center message">
+    <div className="flex message stats">
       <div>
-        <h1 className="center flex">[Chat]</h1>
+        <h2 className="center flex">[Chat]</h2>
 
-        <span className="stack">Users online: </span>
         <span className="stack">Messages: {messages}</span>
-        <span className="stack">Stat:</span>
+
         <FormSure name="chat" onClick={deleteHandler} message={message} />
       </div>
     </div>
