cace92a02d427ffdefc5d277f6b80c033d4f4420
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-14 12:14:24 +0100

    style fix for message-input. when targeted scrolls into view

--- src/Components/FormMessage/index.js
+++ src/Components/FormMessage/index.js
@@ -1,8 +1,19 @@
 import "./styles.css";
+import { useEffect, useRef } from "react";
 
 export default function FormMessage({ content, setContent, submit }) {
+  const messageForm = useRef();
+  useEffect(() => {
+    if (messageForm.target) {
+      document.getElementById("form").scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest",
+      });
+    }
+  }, []);
   return (
-    <form onSubmit={submit} id="form" className="send-form">
+    <form onSubmit={submit} ref={messageForm} id="form" className="send-form">
       <input
         type="text"
         onChange={(e) => setContent(e.target.value)}
