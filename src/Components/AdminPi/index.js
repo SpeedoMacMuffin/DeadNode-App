aa0501665a8d8ddb3fbbaa0b186180f15a440005
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-14 21:14:55 +0100

    style changes

--- src/Components/AdminPi/index.js
+++ src/Components/AdminPi/index.js
@@ -2,9 +2,9 @@ import "./styles.css";
 
 export default function AdminPi({ clients }) {
   return (
-    <div className="one flex center message">
+    <div className="flex center message stats">
       <div>
-        <h1 className="center one flex">[System]</h1>
+        <h2 className="center one flex">[System]</h2>
 
         <span className="stack">Connected Clients: {clients}</span>
         <span className="stack">CPU usage:</span>
