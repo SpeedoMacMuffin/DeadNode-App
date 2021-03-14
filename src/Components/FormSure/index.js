4692744971996f47363c849d4732f6c782011f5a
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-14 21:36:27 +0100

    small style change, changed for to htmlFor

--- src/Components/FormSure/index.js
+++ src/Components/FormSure/index.js
@@ -4,16 +4,16 @@ export default function FormSure({ name, onClick, message }) {
   return (
     <div>
       <label htmlFor={name} className="button flex center danger pseudo">
-        {message}
+        [{message}]
       </label>
 
       <div className="modal ">
         <input id={name} type="checkbox" />
-        <label for={name} className="overlay"></label>
+        <label htmlFor={name} className="overlay"></label>
         <article id="form-sure">
           <header>
             <h3>WARNING!</h3>
-            <label for={name} className="close">
+            <label htmlFor={name} className="close">
               &times;
             </label>
           </header>
@@ -29,7 +29,7 @@ export default function FormSure({ name, onClick, message }) {
             >
               [Delete]
             </label>
-            <label for={name} className="button center danger pseudo half">
+            <label htmlFor={name} className="button center danger pseudo half">
               [Cancel]
             </label>
           </footer>
