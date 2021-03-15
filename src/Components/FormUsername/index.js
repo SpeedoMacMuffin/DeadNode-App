0552034abdd61ceb812daba8f4945a233521d0c3
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-16 00:49:30 +0100

    style & structure change

--- src/Components/FormUsername/index.js
+++ src/Components/FormUsername/index.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { useEffect } from "react";
 
 export default function FormUsername({
   content,
@@ -6,32 +7,53 @@ export default function FormUsername({
   username,
   setUsername,
 }) {
+  useEffect(() => {
+    console.log(localStorage.getItem("username"));
+    const user = localStorage.getItem("username");
+    if (user) {
+      let decodedUser = atob(user);
+      setUsername(decodedUser);
+      console.log(decodedUser);
+    } else {
+      console.log("nope...didn't work");
+    }
+  }, []);
   const validatedContent = (name) => {
     if (name.replace(/\s/g, "") == "") {
       alert("Please enter a username");
     } else {
+      let buff = new Buffer.from(name);
+      let base64user = buff.toString("base64");
       setUsername(name);
+      localStorage.setItem("username", base64user);
     }
   };
   return (
-    <form
-      onSubmit={() => validatedContent(content)}
-      id="form"
-      className="send-form five username-form center"
-    >
-      <input
-        type="text"
-        onChange={(e) => setContent(e.target.value)}
-        value={content}
-        id="text"
-        placeholder="Enter Username..."
-        className="four-fifth message-input"
-        required
-      />
+    <div className="username-view">
+      <div className="one">
+        <h2>Welcome to</h2>
+        <span className="one logo">DeadNode</span>
+        <p>Enter Username to proceed</p>
+      </div>
+      <form
+        onSubmit={() => validatedContent(content)}
+        id="form"
+        className="send-form five username-form center"
+      >
+        <input
+          type="text"
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
+          id="text"
+          placeholder="Enter Username..."
+          className="four-fifth message-input"
+          required
+        />
 
-      <button id="submit" type="submit" className="fifth send-button">
-        Send
-      </button>
-    </form>
+        <button id="submit" type="submit" className="fifth send-button">
+          Send
+        </button>
+      </form>
+    </div>
   );
 }
