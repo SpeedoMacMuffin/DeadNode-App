75668216046ad61b35c3e411db6499a3cc3427c1
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-17 09:40:08 +0100

    added components

--- src/Components/AdminPi/index.js
+++ src/Components/AdminPi/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import FormWifi from "../FormWifi";
+import FormChangePW from "../FormChangePW";
+import FormSurePi from "../FormSurePi";
 
 export default function AdminPi({ clients }) {
   return (
@@ -12,11 +14,23 @@ export default function AdminPi({ clients }) {
         <span className="stack">RAM usage:</span>
         <span className="stack">Wireless: </span>
 
-        <FormWifi name="wifi" message="Wireless Setup" />
-        <button className="pseudo flex center setup">[Change Password]</button>
+        <FormWifi name="wifi" message="Wireless Settings" />
+        <FormChangePW name="changePW" message="Password Settings" />
+        {/* <button className="pseudo flex center setup">[Change Password]</button> */}
         <div className="flex center">
-          <button className="pseudo center danger">[Reboot Pi]</button>
-          <button className="pseudo  center danger">[Shutdown Pi]</button>
+          {/* <button className="pseudo center danger">[Reboot Pi]</button> */}
+          {/* <button className="pseudo  center danger">[Shutdown Pi]</button> */}
+
+          <FormSurePi
+            name="reboot"
+            message="Reboot Pi"
+            content="Are you sure? This will shutdown and reboot the Pi. This might take a while."
+          />
+          <FormSurePi
+            name="shutdown"
+            message="Shutdown Pi"
+            content="Are you sure? This will shutdown the Pi."
+          />
         </div>
       </div>
     </div>
