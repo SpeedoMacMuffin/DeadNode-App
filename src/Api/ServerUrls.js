451079c6e52079f407264114d7d33c250288dd0a
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-17 09:39:38 +0100

    added adminServerUrl

--- src/Api/ServerUrls.js
+++ src/Api/ServerUrls.js
@@ -1,4 +1,5 @@
 const fileServerUrl = "http://localhost:4000";
 const chatServerUrl = "http://localhost:8000";
+const adminServerUrl = "http://localhost:8080";
 
-export { fileServerUrl, chatServerUrl };
+export { fileServerUrl, chatServerUrl, adminServerUrl };
