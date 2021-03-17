0436d3b12e183c175c57f8870dea47f0134cbd88
Stefan Klarmann <klarmann1@googlemail.com>
2021-03-17 09:38:18 +0100

    added form for reboot/shutdown

---
+++ src/Components/FormSurePi/index.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+export default function FormSurePi({ name, onClick, message, content }) {
+  return (
+    <div>
+      <label htmlFor={name} className="button flex center danger pseudo">
+        [{message}]
+      </label>
+
+      <div className="modal ">
+        <input id={name} type="checkbox" />
+        <label htmlFor={name} className="overlay"></label>
+        <article id="form-sure">
+          <header>
+            <h3>WARNING!</h3>
+            <label htmlFor={name} className="close">
+              &times;
+            </label>
+          </header>
+          <section className="content">{content}</section>
+          <footer>
+            <label
+              htmlFor={name}
+              className="button center half pseudo setup"
+              onClick={onClick}
+            >
+              [{message}]
+            </label>
+            <label htmlFor={name} className="button center danger pseudo half">
+              [Cancel]
+            </label>
+          </footer>
+        </article>
+      </div>
+    </div>
+  );
+}
